91d8c18e9539b1e0be48c08c961731d3d92cc1c1
dhrumitwork8 <dhrumitwork8@gmail.com>
2025-11-16 01:31:24 +0530

    add get running api in server.js file after deploy

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ import { buildServer } from "./src/app.js";
 const start = async () => {
   const fastify = await buildServer();
 
+  fastify.get("/", async () => {
+    return {
+      status: "ok",
+      message: "server(zh-graph-server) is running 🚀",
+    };
+  });
+  
   try {
     await fastify.listen({ port: process.env.PORT || 3000, host: "0.0.0.0" });
     fastify.log.info(`Server running on http://localhost:${process.env.PORT}`);
